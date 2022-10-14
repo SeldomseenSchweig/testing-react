49ef8c5ea05994dc353feed5f83852fd8b273e0b
SeldomseenSchweig <jamesschweig@gmail.com>
2022-10-14 12:02:50 -0700

    tests and things done

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, getByTestId } from "@testing-library/react";
 import Carousel from "./Carousel";
 
 
@@ -30,7 +30,7 @@ it("works when you click on the right arrow", function() {
 });
 
 it("works when you click on the left arrow", function() {
-  const { queryByTestId, queryByAltText } = render(<Carousel/>);
+  const { queryByTestId, queryByAltText, debug } = render(<Carousel/>);
   const rightArrow = queryByTestId("right-arrow");
   fireEvent.click(rightArrow);
 
@@ -39,10 +39,35 @@ it("works when you click on the left arrow", function() {
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
 
   // move backward in the carousel
+  
+
   const leftArrow = queryByTestId("left-arrow");
+
   fireEvent.click(leftArrow);
 
   // expect the first image to show, but not the second
   expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
-  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Josh Post on Unsplash")).not.toBeInTheDocument();
 });
+
+
+it("Tests left arrow gone when on first picture", function() {
+  const { queryByTestId} = render(<Carousel />);
+
+  // expect the left arrow to not be there
+  expect(queryByTestId("left-arrow")).not.toBeInTheDocument();
+ 
+});
+
+it("Tests right arrow gone when on last picture", function() {
+  const { queryByTestId} = render(<Carousel />);
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+  expect(queryByTestId("right-arrow")).not.toBeInTheDocument();
+
+
+  // expect the first image to show, but not the second
+ 
+});
+
